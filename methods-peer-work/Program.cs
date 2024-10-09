a0fc6bdffeb3c375b8a3e0603dc3ff6d17aa41dd
Presley-Smith04 <presleysmith04@gmail.com>
2024-10-09 19:22:13 -0400

    file creations

---
+++ methods-peer-work/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace methods_peer_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
