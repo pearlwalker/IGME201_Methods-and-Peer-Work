cb7dda653c76f6496b7b7feadda25591b5409488
Presley-Smith04 <presleysmith04@gmail.com>
2024-10-10 11:20:25 -0400

    Moved Methods to top of program, and made/called subtract method

--- methods-peer-work/Program.cs
+++ methods-peer-work/Program.cs
@@ -4,40 +4,41 @@ namespace methods_peer_work
 {
     internal class Program
     {
+        //partner A add method
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+        //partner B subtract method
+        static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
         static void Main(string[] args)
         {
-                int presleyFavoriteNumber = 0;
-                int pearlFavoriteNumber = 0;
-                
-                //initialize favorite numbers
-                pearlFavoriteNumber = 27;
-                presleyFavoriteNumber = 0;
-
-                //Start of the actual program
-                Console.WriteLine("Pearl's favorite number is " + pearlFavoriteNumber + ".");
-                Console.WriteLine("Presley's favorite number is " + presleyFavoriteNumber + ".");
+            //initialize numbers
+            int presleyFavoriteNumber = 4;
+            int pearlFavoriteNumber = 27;
 
 
-                //Create partner B Subtract method
-                static int Subtract(int num1, int num2)
-                {
-                    int answer = 0;
-                    answer = num1 - num2;
-                    return answer;
-                }
-                //Call Add method
-                int sum = Add(pearlFavoriteNumber, presleyFavoriteNumber);
-                Console.WriteLine("When you add Pearl's favorite number and Presley's favorite number, you get " + sum + "!");
-            }
+            //Start of the actual program
+            Console.WriteLine("Pearl's favorite number is " + pearlFavoriteNumber + ".");
+            Console.WriteLine("Presley's favorite number is " + presleyFavoriteNumber + ".");
 
 
-            //Create partner A Add method
-            static int Add(int num1, int num2)
-            {
-                int answer = 0;
-                answer = num1 + num2;
-                return answer;
-                }
+
+            //Call Add method
+            int sum = Add(pearlFavoriteNumber, presleyFavoriteNumber);
+            Console.WriteLine("When you add Pearl's favorite number and Presley's favorite number, you get " + sum + "!");
+
+            //Call Subtract Method
+            int answer = Subtract(pearlFavoriteNumber, presleyFavoriteNumber);
+            Console.WriteLine("When you subtract Pearl's Favorite number and Presleys Favorite Number, you get " + answer + "!");
+
         }
     }
+}
 
--- methods-peer-work/obj/Debug/net8.0/methods-peer-work.AssemblyInfo.cs
+++ methods-peer-work/obj/Debug/net8.0/methods-peer-work.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("methods-peer-work")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1683a34312a91fe1ffeeff9f0edfdbe0fa001eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f0faed23b1bd2592a04aafe950402d32f39953")]
 [assembly: System.Reflection.AssemblyProductAttribute("methods-peer-work")]
 [assembly: System.Reflection.AssemblyTitleAttribute("methods-peer-work")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
